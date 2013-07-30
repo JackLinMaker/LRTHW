66f2f22ee381d0b8ce2405e6f8c92e8e402bcf37
jacklinmaker <jacklinmaker@gmail.com>
2013-07-30 12:13:30 +0800

    finish the ex40.rb

---
+++ MyStuff.rb
@@ -0,0 +1,12 @@
+class MyStuff
+  def initialize()
+    @tanerine = "And now a thousand years between"
+  end
+  
+  attr_reader :tangerine
+  
+  def apple()
+    puts "I am classy apple!"
+  end
+end
+
